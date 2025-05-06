a98fe1feea24b0076fbe65c1175bb5318232ce65
Abcxyz1234107 <minecraftzcc6@gmail.com>
2025-05-06 10:59:51 +0700

    Sửa lại bbox của Pipe

--- 05-SceneManager/Pipe.h
+++ 05-SceneManager/Pipe.h
@@ -7,18 +7,21 @@ class CPipe : public CGameObject
 private:
 	CPipeBody* body;
 	CPipeHead* head;
+    float totalWidth; //Total width of Body only.
+    float totalHeight;
 public:
 	CPipe(float x, float y,
         int bodyLength, float cellWidth = 30, float cellHeight = 16,
-        int spriteId = 21002)
+        int spriteId = ID_SPRITE_PIPEBODY)
         : CGameObject(x, y)
     {
-        z = 2;
-        this->head = new CPipeHead(x, y);
-        this->body = new CPipeBody(x+0.75f, y, cellWidth, cellHeight, bodyLength, spriteId);
+        this->head = new CPipeHead(x-1, y);
+        this->body = new CPipeBody(x, y, cellWidth, cellHeight, bodyLength, spriteId);
+        totalHeight = cellHeight * bodyLength;
+        totalWidth = cellWidth;
     }
     void Render();
-    void GetBoundingBox(float& l, float& t, float& r, float& b) {}
+    void GetBoundingBox(float& l, float& t, float& r, float& b);
 
     CPipeHead* GetHead() { return head; }
     CPipeBody* GetBody() { return body; }
