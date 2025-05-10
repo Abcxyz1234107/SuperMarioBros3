3b849d53f453b9b3cc81140dfb48d121ea5d6355
Abcxyz1234107 <minecraftzcc6@gmail.com>
2025-05-10 22:09:24 +0700

    Định nghĩa các hàm của randomLeaf (bị lỗi crash khi spawn từ gạch)

--- 05-SceneManager/RandomLeaf.cpp
+++ 05-SceneManager/RandomLeaf.cpp
@@ -9,7 +9,25 @@ void CRandomLeaf::Render()
 
 void CRandomLeaf::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	vy += 0.0005f * dt;
+	if (state == LEAF_STATE_RISING)
+	{
+		vy += LEAF_GRAVITY * dt;
+		if (vy >= 0)   // đã đạt đỉnh, chuyển sang rơi
+		{
+			vy = LEAF_FALL_SPEED_MAX;
+			SetState(LEAF_STATE_FALLING_LEFT);
+		}
+	}
+	else  // đang lượn
+	{
+		vy += LEAF_GRAVITY * dt;
+		if (vy > LEAF_FALL_SPEED_MAX) vy = LEAF_FALL_SPEED_MAX;
+
+		if (state == LEAF_STATE_FALLING_LEFT && x <= xPivot - LEAF_AMPLITUDE)
+			SetState(LEAF_STATE_FALLING_RIGHT);
+		else if (state == LEAF_STATE_FALLING_RIGHT && x >= xPivot + LEAF_AMPLITUDE)
+			SetState(LEAF_STATE_FALLING_LEFT);
+	}
 
 	CGameObject::Update(dt, coObjects);
 	CCollision::GetInstance()->Process(this, dt, coObjects);
@@ -17,6 +35,7 @@ void CRandomLeaf::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 
 void CRandomLeaf::OnNoCollision(DWORD dt)
 {
+	x += vx * dt;
 	y += vy * dt;
 }
 
@@ -31,4 +50,26 @@ void CRandomLeaf::GetBoundingBox(float& l, float& t, float& r, float& b)
 	t = y - LEAF_BBOX_H / 2;
 	r = l + LEAF_BBOX_W;
 	b = t + LEAF_BBOX_H;
+}
+
+void CRandomLeaf::SetState(int state)
+{
+	this->state = state;
+
+	switch (state)
+	{
+	case LEAF_STATE_RISING:
+		vx = 0.0f;
+		break;
+
+	case LEAF_STATE_FALLING_LEFT:
+		vx = -LEAF_FALL_SPEED_X;
+		xPivot = x;
+		break;
+
+	case LEAF_STATE_FALLING_RIGHT:
+		vx = LEAF_FALL_SPEED_X;
+		xPivot = x;
+		break;
+	}
 }
--- 05-SceneManager/RandomLeaf.h
+++ 05-SceneManager/RandomLeaf.h
@@ -1,16 +1,34 @@
-#pragma once
+﻿#pragma once
 #include "GameObject.h"
 
 #define LEAF_BBOX_W 16
 #define LEAF_BBOX_H 14
 
+#define LEAF_GRAVITY           0.0002f
+#define LEAF_RISE_SPEED        0.25f
+#define LEAF_FALL_SPEED_MAX    0.05f
+#define LEAF_FALL_SPEED_X      0.04f // tốc độ lượn ngang
+#define LEAF_AMPLITUDE         24.0f // biên độ dao động trái-phải
+
+#define LEAF_STATE_RISING          0
+#define LEAF_STATE_FALLING_LEFT    1
+#define LEAF_STATE_FALLING_RIGHT   2
+
+
 class CRandomLeaf :
     public CGameObject
 {
+private:
+	int   state;
+	float xPivot;  // gốc để đo biên dao động
+
 public:
     CRandomLeaf(float x, float y) : CGameObject(x, y)
     {
-        
+		state = LEAF_STATE_RISING;
+		xPivot = x;
+		vx = 0.0f;
+		vy = -LEAF_RISE_SPEED;
     }
 
     void Render() override;
@@ -21,5 +39,7 @@ public:
 
 	void OnNoCollision(DWORD dt) override;
 	void OnCollisionWith(LPCOLLISIONEVENT e) override;
+
+	void SetState(int state) override;
 };
 
